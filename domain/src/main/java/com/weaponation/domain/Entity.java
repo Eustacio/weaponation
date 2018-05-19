fa06efff61e9b38684f4dbb3f2d676e8d9903075
Eustácio <wallison.gth@gmail.com>
2018-05-19 17:18:51 -0300

    Added the class "Entity" to the "domain" module, to be the superclass of all entities, providing common fields to all your descendants

---
+++ domain/src/main/java/com/weaponation/domain/Entity.java
@@ -0,0 +1,23 @@
+package com.weaponation.domain;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+/**
+ * @author Wallison Freitas
+ */
+@MappedSuperclass
+public abstract class Entity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+}
