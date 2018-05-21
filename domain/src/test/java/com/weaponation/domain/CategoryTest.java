ce4a1ae7973c27b985f304800bf656452e859b7c
Eustácio <wallison.gth@gmail.com>
2018-05-21 18:59:27 -0300

    Fixed the CategoryTest#equalsAndHashCodeContract method, this close #1

--- domain/src/test/java/com/weaponation/domain/CategoryTest.java
+++ domain/src/test/java/com/weaponation/domain/CategoryTest.java
@@ -3,6 +3,8 @@ package com.weaponation.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 import nl.jqno.equalsverifier.EqualsVerifier;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,7 +17,18 @@ class CategoryTest {
     @Test
     @DisplayName("equals() and hashCode() contract")
     void equalsAndHashCodeContract() {
+        final Product dummyProduct1 = new Product("someProduct",
+                "some description", "some specification", BigDecimal.ZERO);
+
+        final Product dummyProduct2 = new Product("anotherProduct",
+                "another description", "some specification", BigDecimal.ONE);
+
         EqualsVerifier.forClass(Category.class)
+                // Add prefab values to avoid java.lang.AssertionError: Recursive datastructure.
+                // To More info check http://jqno.nl/equalsverifier/errormessages/recursive-datastructure/
+                .withPrefabValues(Product.class, dummyProduct1, dummyProduct2)
+
+                // The "name" field is the only field that is used in equals() and hashCode() methods
                 .withOnlyTheseFields("name")
                 .verify();
     }
