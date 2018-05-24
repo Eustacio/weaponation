7da06af04603456ca68a4602bf9e11272400c4ee
Eustácio <wallison.gth@gmail.com>
2018-05-24 08:47:54 -0300

    Added the "ProductRepository" class to the "repository" module, to retrieve information of the "Product" entity

---
+++ repository/src/main/java/com/weaponation/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.weaponation.repository;
+
+import com.weaponation.domain.Product;
+
+/**
+ * @author Wallison Freitas
+ */
+public interface ProductRepository extends EntityRepository<Product> {}
