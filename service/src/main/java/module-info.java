6e757c49a2e1843d889efb34532c60fd6e6089e0
Eustácio <wallison.gth@gmail.com>
2018-05-25 14:56:54 -0300

    Added "requires" clause to the module-info.java file of the "service" module, to be able to use the spring.context and the spring.beans packages

--- service/src/main/java/module-info.java
+++ service/src/main/java/module-info.java
@@ -1,4 +1,10 @@
 module com.weaponation.service {
     requires com.weaponation.domain;
     requires com.weaponation.repository;
+
+    // Necessary to use @Service annotation
+    requires spring.context;
+
+    // Necessary to use @Autowired annotation
+    requires spring.beans;
 }
