09877361126aef780c658ac71c2354a9971eb3cc
Eustácio <wallison.gth@gmail.com>
2018-05-26 13:48:34 -0300

    Added the "DataSourceConfig" class to the "repository" module, to configure the all DataSource used in the application

---
+++ repository/src/main/java/com/weaponation/config/DataSourceConfig.java
@@ -0,0 +1,28 @@
+package com.weaponation.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import java.nio.charset.StandardCharsets;
+
+import javax.sql.DataSource;
+
+/**
+ * @author Wallison Freitas
+ */
+@Configuration
+public class DataSourceConfig {
+
+    @Bean("dataSource")
+    @Profile("development")
+    public DataSource developmentDataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setName("embedded_database")
+                .setType(EmbeddedDatabaseType.H2)
+                .setScriptEncoding(StandardCharsets.UTF_8.name())
+                .build();
+    }
+}
