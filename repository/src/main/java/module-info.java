6c95fa7d3c5a0afae1fc068a6c09b75bd0a5b4df
Eustácio <wallison.gth@gmail.com>
2018-05-26 13:21:21 -0300

    Added "requires" clause to the module-info.java file of the "repository" module, to be able to use the java.persistence, java.sql, spring.context, spring.data.jpa, spring.jdbc, spring.orm and spring.tx packages

--- repository/src/main/java/module-info.java
+++ repository/src/main/java/module-info.java
@@ -1,6 +1,14 @@
 module com.weaponation.repository {
     requires com.weaponation.domain;
+
+    requires java.persistence;
+    requires java.sql;
+
+    requires spring.context;
     requires spring.data.jpa;
+    requires spring.jdbc;
+    requires spring.orm;
+    requires spring.tx;
 
     exports com.weaponation.repository;
 }
