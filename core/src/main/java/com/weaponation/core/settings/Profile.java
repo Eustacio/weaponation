8e58ba35ec365fe7ccf3a1744861d81e60e77584
Eustácio <wallison.gth@gmail.com>
2018-05-27 15:51:35 -0300

    Added the "Profile" class to the "core" module, to store all profiles that the application can have

---
+++ core/src/main/java/com/weaponation/core/settings/Profile.java
@@ -0,0 +1,10 @@
+package com.weaponation.core.settings;
+
+/**
+ * @author Wallison Freitas
+ */
+public abstract class Profile {
+
+    public static final String DEVELOPMENT = "development";
+    public static final String INTEGRATION_TEST = "integration-test";
+}
