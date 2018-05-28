16d51852940b76f6a2dc484e84ce8762cc212c14
Eustácio <wallison.gth@gmail.com>
2018-05-28 07:03:41 -0300

    Added the "RootApplicationContext" class to the "core" module, to enable the Spring component scan

---
+++ core/src/main/java/com/weaponation/core/config/RootApplicationContext.java
@@ -0,0 +1,11 @@
+package com.weaponation.core.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Wallison Freitas
+ */
+@Configuration
+@ComponentScan(basePackages = "com.weaponation")
+public class RootApplicationContext {}
