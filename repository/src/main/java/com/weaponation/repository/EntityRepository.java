480d1a985ba4f643dc574bc5ef05c264ba943929
Eustácio <wallison.gth@gmail.com>
2018-05-29 16:09:46 -0300

    Added "@NoRepositoryBean" annotation to the "EntityRepository" interface of the "repository" module, to exclude the repository interfaces from being picked up and thus in consequence getting an instance being created

--- repository/src/main/java/com/weaponation/repository/EntityRepository.java
+++ repository/src/main/java/com/weaponation/repository/EntityRepository.java
@@ -1,8 +1,10 @@
 package com.weaponation.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
 /**
  * @author Wallison Freitas
  */
+@NoRepositoryBean
 public interface EntityRepository<T> extends JpaRepository<T, Long> {}
