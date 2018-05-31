23211897f2f9375055754146a0ff8fcb2309c3d2
Eustácio <wallison.gth@gmail.com>
2018-05-31 14:12:45 -0300

    Changed the "EntityService" class of the "service" module, to implements the "save" method by itself, instead of leave to the child classes implement it

--- service/src/main/java/com/weaponation/service/EntityService.java
+++ service/src/main/java/com/weaponation/service/EntityService.java
@@ -1,9 +1,13 @@
 package com.weaponation.service;
 
 import com.weaponation.domain.BaseEntity;
+import com.weaponation.exception.DuplicateEntityException;
 import com.weaponation.repository.EntityRepository;
 
+import org.springframework.dao.DataIntegrityViolationException;
+
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 /**
@@ -17,7 +21,16 @@ public abstract class EntityService<T extends BaseEntity> {
         this.repository = repository;
     }
 
-    public abstract T save(T entity);
+    public T save(T entity) {
+        Objects.requireNonNull(entity, "Entity cannot be null!");
+
+        try {
+            return repository.save(entity);
+        } catch (DataIntegrityViolationException dataIntegrityException) {
+            throw new DuplicateEntityException("Cannot save the '" + entity.getClass().getName() +
+                    "' because it already exists!", dataIntegrityException);
+        }
+    }
 
     public abstract boolean delete(Long id);
 
