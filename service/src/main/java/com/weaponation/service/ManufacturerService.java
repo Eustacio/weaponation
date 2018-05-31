f496906960ef0ff8ba55eaf6bb1e6406525060f9
Eustácio <wallison.gth@gmail.com>
2018-05-31 14:21:18 -0300

    Removed the "save" and "delete" methods of the "ManufacturerService" class of the "service" module, because now its are implemented in the superclass

--- service/src/main/java/com/weaponation/service/ManufacturerService.java
+++ service/src/main/java/com/weaponation/service/ManufacturerService.java
@@ -1,13 +1,9 @@
 package com.weaponation.service;
 
 import com.weaponation.domain.Manufacturer;
-import com.weaponation.exception.DuplicateEntityException;
-import com.weaponation.exception.EntityNotFoundException;
 import com.weaponation.repository.ManufacturerRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 /**
@@ -19,31 +15,5 @@ public class ManufacturerService extends EntityService<Manufacturer> {
     @Autowired
     public ManufacturerService(ManufacturerRepository repository) {
         super(repository);
-    }
-
-    @Override
-    public Manufacturer save(Manufacturer manufacturer) {
-        Manufacturer savedManufacturer;
-
-        try {
-            savedManufacturer = super.repository.save(manufacturer);
-        } catch (DataIntegrityViolationException dataIntegrityException) {
-            throw new DuplicateEntityException("Cannot save the manufacturer '" + manufacturer.getName() +
-                    "' because it already exists!", dataIntegrityException);
-        }
-
-        return savedManufacturer;
-    }
-
-    @Override
-    public boolean delete(Long id) {
-        try {
-            super.repository.deleteById(id);
-        } catch (EmptyResultDataAccessException emptyResultDataAccessException) {
-            throw new EntityNotFoundException("Cannot delete the Manufacturer with id '" + id +
-                    "' because it does not exists", emptyResultDataAccessException);
-        }
-
-        return !super.repository.existsById(id);
     }
 }
