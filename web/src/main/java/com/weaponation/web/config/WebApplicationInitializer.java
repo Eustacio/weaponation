2db9d67f84728f5a905fd9953ada8bbff7ffd9ca
Eustácio <wallison.gth@gmail.com>
2018-06-02 14:15:06 -0300

    Added the "WebApplicationInitializer" class to the "web" module, to register a DispatcherServlet and use Java-based Spring configuration

---
+++ web/src/main/java/com/weaponation/web/config/WebApplicationInitializer.java
@@ -0,0 +1,26 @@
+package com.weaponation.web.config;
+
+import com.weaponation.core.config.RootApplicationContext;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * @author Wallison Freitas
+ */
+public class WebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{RootApplicationContext.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebApplicationContext.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
