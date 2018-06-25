9241cd789aa2c5329a6ceedb4a021b70f532d7a1
Eustácio <wallison.gth@gmail.com>
2018-06-25 16:48:21 -0300

    Added the "CoreModule" to the "imports" section of the "app.module.ts" file, to store single use components

--- web/src/main/webapp/src/app/app.module.ts
+++ web/src/main/webapp/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { CoreModule } from './core/core.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    CoreModule
+  ],
   declarations: [
     AppComponent
-  ],
-  imports: [
-    BrowserModule
   ],
   bootstrap: [AppComponent]
 })
