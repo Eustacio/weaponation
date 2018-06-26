1070cedf3f97c7c7f4a1511cb06f5187354a07dc
Eustácio <wallison.gth@gmail.com>
2018-06-26 16:10:16 -0300

    Created the "BannerComponent" and added it to the "declarations" section of the "core.module.ts" file

--- web/src/main/webapp/src/app/core/core.module.ts
+++ web/src/main/webapp/src/app/core/core.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 import { MenubarModule } from 'primeng/menubar';
 
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   imports: [
     CommonModule,
     MenubarModule
   ],
-  declarations: [ToolbarComponent]
+  declarations: [
+    ToolbarComponent,
+    BannerComponent
+  ]
 })
 export class CoreModule {}
