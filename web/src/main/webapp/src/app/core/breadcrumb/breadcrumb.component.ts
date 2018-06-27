f419037db2ced3e2ebbbd5bdd7edd9f16c870adc
Eustácio <wallison.gth@gmail.com>
2018-06-27 16:16:04 -0300

    Changed the "breadcrumb.component.ts" to show a fake path during the development process

--- web/src/main/webapp/src/app/core/breadcrumb/breadcrumb.component.ts
+++ web/src/main/webapp/src/app/core/breadcrumb/breadcrumb.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-breadcrumb',
   templateUrl: './breadcrumb.component.html',
   styleUrls: ['./breadcrumb.component.css']
 })
-export class BreadcrumbComponent {
+export class BreadcrumbComponent implements OnInit {
 
+  homeMenuItem: MenuItem;
+  menuItems: MenuItem[];
+
+  ngOnInit(): void {
+    this.homeMenuItem = { icon: 'pi pi-home' };
+
+    // TODO: fill the array with information from the actual URL
+    this.menuItems = [
+      { label: 'Firearms' },
+      { label: 'Rifles' }
+    ];
+  }
 }
