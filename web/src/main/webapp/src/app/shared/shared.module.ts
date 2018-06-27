9c2f4fd42dcff39c56e104e55ffe4fb0370db8c1
Eustácio <wallison.gth@gmail.com>
2018-06-27 17:40:43 -0300

    Added the PrimeNG "CarouselModule" to the "imports" section of the "shared.module.ts" file

--- web/src/main/webapp/src/app/shared/shared.module.ts
+++ web/src/main/webapp/src/app/shared/shared.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CarouselModule } from 'primeng/carousel';
+
 import { ShowcaseComponent } from './showcase/showcase.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    CarouselModule
   ],
   declarations: [
     ShowcaseComponent
