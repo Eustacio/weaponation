efbb09c837b304f74cc9354dc4f87bc6149f67ce
Eustácio <wallison.gth@gmail.com>
2018-06-27 13:46:22 -0300

    Changed the "BannerComponent" to show the banner with the previously committed images

--- web/src/main/webapp/src/app/core/banner/banner.component.ts
+++ web/src/main/webapp/src/app/core/banner/banner.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { BannerImage } from './banner-image';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.css']
 })
-export class BannerComponent {
+export class BannerComponent implements OnInit {
 
+  // Number of visible images at same time
+  readonly visibleImages = 1;
+
+  // Time between the image change (in milliseconds)
+  readonly autoplayInterval = 4000;
+
+  // The images of the banner
+  bannerImages: BannerImage[];
+
+  ngOnInit(): void {
+    // TODO: fetch from database
+    this.bannerImages = [
+      { imageName: '1911.jpg' },
+      { imageName: 'ak47.jpg' },
+      { imageName: 'ar15_tactical.jpg' },
+      { imageName: 'five_seven.jpg' },
+      { imageName: 'granede_launcher.jpg' }
+    ];
+  }
+
+  /**
+   * @return Number of maximum page links to display (wathe little dots above the image).
+   * If total page count exceeds this value a dropdown is displayed instead.
+   * */
+  get maxPageLinks(): number {
+    return this.bannerImages.length;
+  }
 }
