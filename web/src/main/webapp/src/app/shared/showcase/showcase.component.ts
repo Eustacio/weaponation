8b9f373831fc6d30bf88458a3999e75878881698
Eustácio <wallison.gth@gmail.com>
2018-06-28 16:17:24 -0300

    Implemented the "ShowcaseComponent", to be able to receive external data to be properly displayed by the component

--- web/src/main/webapp/src/app/shared/showcase/showcase.component.ts
+++ web/src/main/webapp/src/app/shared/showcase/showcase.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { Product } from './product';
 
 @Component({
   selector: 'app-showcase',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./showcase.component.css']
 })
 export class ShowcaseComponent {
+
+  // Number of maximum page links to display (the little dots above the image)
+  readonly pageLinks = 3;
+
+  // If the carousel will play automatically
+  @Input() circular: boolean = true;
+
+  // Time between the image change (in milliseconds)
+  @Input() autoPlayInterval: number = 4000;
+
+  // The images that are displayed
+  @Input() products: Product[];
 
 }
