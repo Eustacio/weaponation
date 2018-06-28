de325868d9f50ad920653b3fea4a7558860ac0e0
Eustácio <wallison.gth@gmail.com>
2018-06-28 16:23:57 -0300

    Added PrimeNG "ButtonModule" to the "imports" section of the "shared.module.ts" file

--- web/src/main/webapp/src/app/shared/shared.module.ts
+++ web/src/main/webapp/src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CarouselModule } from 'primeng/carousel';
+import { ButtonModule } from 'primeng/button';
 
 import { ShowcaseComponent } from './showcase/showcase.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    CarouselModule
+    CarouselModule,
+    ButtonModule
   ],
   declarations: [
     ShowcaseComponent
