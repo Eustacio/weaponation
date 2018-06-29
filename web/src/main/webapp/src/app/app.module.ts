ccccd4cce59a3d88c2c250d41f311ce2df4b3259
Eustácio <wallison.gth@gmail.com>
2018-06-29 17:54:58 -0300

    Added the "SharedModule" to the "imports" section of the "app.module.ts" file

--- web/src/main/webapp/src/app/app.module.ts
+++ web/src/main/webapp/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { CoreModule } from './core/core.module';
 
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './core/toolbar/toolbar.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   imports: [
     BrowserModule,
-    CoreModule
+    CoreModule,
+    SharedModule
   ],
   declarations: [
     AppComponent
