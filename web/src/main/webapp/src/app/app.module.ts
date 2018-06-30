3e51254a7ffb598fa10811c1760e9c59defea6c6
Eustácio <wallison.gth@gmail.com>
2018-06-30 16:56:10 -0300

    Added the "app-routing.module.ts" file, and added it to the "imports" section of the "app.module.ts" file

--- web/src/main/webapp/src/app/app.module.ts
+++ web/src/main/webapp/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { CoreModule } from './core/core.module';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    CoreModule
+    CoreModule,
+    AppRoutingModule
   ],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
