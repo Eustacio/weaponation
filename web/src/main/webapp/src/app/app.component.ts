98984835976fa2c09e914bd526688a885112359c
Eustácio <wallison.gth@gmail.com>
2018-07-01 08:31:20 -0300

    Removed the home page fake data from the "app.component.ts" file, and add it to the "home.component.ts" file

--- web/src/main/webapp/src/app/app.component.ts
+++ web/src/main/webapp/src/app/app.component.ts
@@ -1,50 +1,9 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 
-import { Product } from './shared/showcase/product';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
-
-  // TODO: fetch all this information from the service
-  handguns: Product[] = [
-    { name: 'p1', description: 'description 1', price: '$1234.1', imageName: '1911.jpg' },
-    { name: 'p2', description: 'description 2', price: '$1234.1', imageName: '1911.jpg' },
-    { name: 'p3', description: 'description 3', price: '$1234.1', imageName: '1911.jpg' },
-    { name: 'p1', description: 'description 1', price: '$1234.1', imageName: '1911.jpg' },
-    { name: 'p2', description: 'description 2', price: '$1234.1', imageName: '1911.jpg' },
-    { name: 'p3', description: 'description 3', price: '$1234.1', imageName: '1911.jpg' },
-    { name: 'p1', description: 'description 1', price: '$1234.1', imageName: '1911.jpg' },
-    { name: 'p2', description: 'description 2', price: '$1234.1', imageName: '1911.jpg' },
-    { name: 'p3', description: 'description 3', price: '$1234.1', imageName: '1911.jpg' }
-  ];
-
-  shotguns: Product[] = [
-    { name: 'p1', description: 'description 1', price: '$1234.1', imageName: 'granede_launcher.jpg' },
-    { name: 'p2', description: 'description 2', price: '$1234.1', imageName: 'granede_launcher.jpg' },
-    { name: 'p3', description: 'description 3', price: '$1234.1', imageName: 'granede_launcher.jpg' },
-    { name: 'p1', description: 'description 1', price: '$1234.1', imageName: 'granede_launcher.jpg' },
-    { name: 'p2', description: 'description 2', price: '$1234.1', imageName: 'granede_launcher.jpg' },
-    { name: 'p3', description: 'description 3', price: '$1234.1', imageName: 'granede_launcher.jpg' },
-    { name: 'p1', description: 'description 1', price: '$1234.1', imageName: 'granede_launcher.jpg' },
-    { name: 'p2', description: 'description 2', price: '$1234.1', imageName: 'granede_launcher.jpg' },
-    { name: 'p3', description: 'description 3', price: '$1234.1', imageName: 'granede_launcher.jpg' }
-  ];
-
-  rifles: Product[] = [
-    { name: 'p1', description: 'description 1', price: '$1234.1', imageName: 'ar15_tactical.jpg' },
-    { name: 'p2', description: 'description 2', price: '$1234.1', imageName: 'ak47.jpg' },
-    { name: 'p3', description: 'description 3', price: '$1234.1', imageName: 'ak47.jpg' },
-    { name: 'p1', description: 'description 1', price: '$1234.1', imageName: 'ar15_tactical.jpg' },
-    { name: 'p2', description: 'description 2', price: '$1234.1', imageName: 'ak47.jpg' },
-    { name: 'p3', description: 'description 3', price: '$1234.1', imageName: 'ak47.jpg' },
-    { name: 'p1', description: 'description 1', price: '$1234.1', imageName: 'ar15_tactical.jpg' },
-    { name: 'p2', description: 'description 2', price: '$1234.1', imageName: 'ak47.jpg' },
-    { name: 'p3', description: 'description 3', price: '$1234.1', imageName: 'ak47.jpg' }
-  ];
-
-}
+export class AppComponent {}
--- web/src/main/webapp/src/app/pages/home/home.component.ts
+++ web/src/main/webapp/src/app/pages/home/home.component.ts
@@ -1,8 +1,49 @@
 import { Component } from '@angular/core';
 
+import { Product } from '../../shared/showcase/product';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {}
+export class HomeComponent {
+
+
+  // TODO: fetch all this information from the service
+  handguns: Product[] = [
+    { name: 'p1', description: 'description 1', price: '$1234.1', imageName: '1911.jpg' },
+    { name: 'p2', description: 'description 2', price: '$1234.1', imageName: '1911.jpg' },
+    { name: 'p3', description: 'description 3', price: '$1234.1', imageName: '1911.jpg' },
+    { name: 'p1', description: 'description 1', price: '$1234.1', imageName: '1911.jpg' },
+    { name: 'p2', description: 'description 2', price: '$1234.1', imageName: '1911.jpg' },
+    { name: 'p3', description: 'description 3', price: '$1234.1', imageName: '1911.jpg' },
+    { name: 'p1', description: 'description 1', price: '$1234.1', imageName: '1911.jpg' },
+    { name: 'p2', description: 'description 2', price: '$1234.1', imageName: '1911.jpg' },
+    { name: 'p3', description: 'description 3', price: '$1234.1', imageName: '1911.jpg' }
+  ];
+
+  shotguns: Product[] = [
+    { name: 'p1', description: 'description 1', price: '$1234.1', imageName: 'granede_launcher.jpg' },
+    { name: 'p2', description: 'description 2', price: '$1234.1', imageName: 'granede_launcher.jpg' },
+    { name: 'p3', description: 'description 3', price: '$1234.1', imageName: 'granede_launcher.jpg' },
+    { name: 'p1', description: 'description 1', price: '$1234.1', imageName: 'granede_launcher.jpg' },
+    { name: 'p2', description: 'description 2', price: '$1234.1', imageName: 'granede_launcher.jpg' },
+    { name: 'p3', description: 'description 3', price: '$1234.1', imageName: 'granede_launcher.jpg' },
+    { name: 'p1', description: 'description 1', price: '$1234.1', imageName: 'granede_launcher.jpg' },
+    { name: 'p2', description: 'description 2', price: '$1234.1', imageName: 'granede_launcher.jpg' },
+    { name: 'p3', description: 'description 3', price: '$1234.1', imageName: 'granede_launcher.jpg' }
+  ];
+
+  rifles: Product[] = [
+    { name: 'p1', description: 'description 1', price: '$1234.1', imageName: 'ar15_tactical.jpg' },
+    { name: 'p2', description: 'description 2', price: '$1234.1', imageName: 'ak47.jpg' },
+    { name: 'p3', description: 'description 3', price: '$1234.1', imageName: 'ak47.jpg' },
+    { name: 'p1', description: 'description 1', price: '$1234.1', imageName: 'ar15_tactical.jpg' },
+    { name: 'p2', description: 'description 2', price: '$1234.1', imageName: 'ak47.jpg' },
+    { name: 'p3', description: 'description 3', price: '$1234.1', imageName: 'ak47.jpg' },
+    { name: 'p1', description: 'description 1', price: '$1234.1', imageName: 'ar15_tactical.jpg' },
+    { name: 'p2', description: 'description 2', price: '$1234.1', imageName: 'ak47.jpg' },
+    { name: 'p3', description: 'description 3', price: '$1234.1', imageName: 'ak47.jpg' }
+  ];
+}
