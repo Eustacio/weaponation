8f40e1fe37cd088b0184044f6fad0960889a3485
Eustácio <wallison.gth@gmail.com>
2018-07-04 16:02:05 -0300

    Added "requires" clause to the module-info.java file of the "web" module, to be able to use the com.weaponation.service and the spring.beans packages

--- web/src/main/java/module-info.java
+++ web/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.weaponation.web {
     requires com.weaponation.core;
+    requires com.weaponation.service;
 
+    requires spring.beans;
     requires spring.context;
     requires spring.web;
     requires spring.webmvc;
