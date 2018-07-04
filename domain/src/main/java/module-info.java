df1141b384c4a1010e67f622ae58eea61cd1121e
Eustácio <wallison.gth@gmail.com>
2018-07-04 15:24:39 -0300

    Added "requires" clause to the module-info.java file of the "domain" module, to be able to use the com.fasterxml.jackson.annotation package

--- domain/src/main/java/module-info.java
+++ domain/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.weaponation.domain {
     requires java.persistence;
     requires java.validation;
+    requires com.fasterxml.jackson.annotation;
 
     exports com.weaponation.domain;
 }
