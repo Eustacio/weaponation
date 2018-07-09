8b1a0e2de3779ef1c01b77df5ac40ee84f6683b5
Eustácio <wallison.gth@gmail.com>
2018-07-09 16:38:00 -0300

    Added the method "fromStatusCode" to the "service-error.ts" file, to translate an HTTP status code to an instance of the "ServiceError" class

--- web/src/main/webapp/src/app/service/error/service-error.ts
+++ web/src/main/webapp/src/app/service/error/service-error.ts
@@ -4,4 +4,11 @@ export class ServiceError {
 
   constructor(public readonly errorType: ErrorType,
               public readonly message: string) {}
+
+  static fromStatusCode(httpStatusCode: number): ServiceError | null {
+    switch (httpStatusCode) {
+      case 504:
+        return new ServiceError(ErrorType.GATEWAY_TIMEOUT, 'Gateway Timeout');
+    }
+  }
 }
