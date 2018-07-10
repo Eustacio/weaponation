69ab13c73ff2fcf10236c7eb9a95a8b9e2e8e97c
Eustácio <wallison.gth@gmail.com>
2018-07-10 09:04:02 -0300

    Changed the "ProductService" class to extends the "EntityService" class

--- web/src/main/webapp/src/app/service/product.service.ts
+++ web/src/main/webapp/src/app/service/product.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@angular/core';
 
 import { ServiceModule } from './service.module';
 
+import { EntityService } from './entity-service';
+import { Product } from '../model/product';
+
 @Injectable({
   providedIn: ServiceModule
 })
-export class ProductService {}
+export class ProductService extends EntityService<Product> {}
