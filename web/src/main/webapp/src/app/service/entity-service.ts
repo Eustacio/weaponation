e96f14b5dbed97abe462a652e8e555343bd790b8
Eustácio <wallison.gth@gmail.com>
2018-07-10 09:14:29 -0300

    Added the constructor for the "EntityService" TypeScript class

--- web/src/main/webapp/src/app/service/entity-service.ts
+++ web/src/main/webapp/src/app/service/entity-service.ts
@@ -1,3 +1,29 @@
-import { Entity } from '../model/entity';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject, Subject } from 'rxjs';
 
-export abstract class EntityService<T extends Entity> {}
+import { Entity } from '../model/entity';
+import { ServiceError } from './error/service-error';
+import { ServiceEvent } from './event/service-event.enum';
+
+export abstract class EntityService<T extends Entity> {
+
+  // Http header for JSON requests
+  protected readonly jsonHttpHeader: HttpHeaders;
+
+  // BehaviorSubject used to publish data retrieved from the server
+  private dataSubject: BehaviorSubject<T[]>;
+
+  // Subject used to publish events
+  private eventSubject: Subject<ServiceEvent>;
+
+  // Subject used to publish server response errors
+  private errorSubject: Subject<ServiceError>;
+
+  protected constructor(private _httpClient: HttpClient,
+                        protected endpointUrl: string) {
+    this.jsonHttpHeader = new HttpHeaders({ 'Content-Type': 'application/json' });
+    this.dataSubject = new BehaviorSubject([]);
+    this.eventSubject = new Subject();
+    this.errorSubject = new Subject();
+  }
+}
