2c9f2bff9c6d47d1ea6dc547bc5ed5808a1f342e
Eustácio <wallison.gth@gmail.com>
2018-07-11 13:03:38 -0300

    Created the ProductController class, to make CRUD operations on the class "Product" of the "domain" module

---
+++ web/src/main/java/com/weaponation/web/controller/ProductController.java
@@ -0,0 +1,9 @@
+package com.weaponation.web.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Wallison Freitas
+ */
+@RestController
+public class ProductController {}
