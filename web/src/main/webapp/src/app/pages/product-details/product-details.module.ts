dee2eb27fffbd97b986d897931aa49f8b4cbf040
Eustácio <wallison.gth@gmail.com>
2018-07-17 12:57:08 -0300

    Added the "ButtonModule", "TableModule" and "SharedModule" to the "imports" section of the "product-details.module.ts" file

--- web/src/main/webapp/src/app/pages/product-details/product-details.module.ts
+++ web/src/main/webapp/src/app/pages/product-details/product-details.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
 
 import { ProductDetailsRoutingModule } from './product-details-routing.module';
+import { SharedModule } from '../../shared/shared.module';
 
 import { ProductDetailsComponent } from './product-details.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    ButtonModule,
+    TableModule,
+    SharedModule,
     ProductDetailsRoutingModule
   ],
   declarations: [ProductDetailsComponent]
