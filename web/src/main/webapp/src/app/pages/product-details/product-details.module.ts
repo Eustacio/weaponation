a0b040f682368aa1c6bd60399129851ca5b2334d
Eustácio <wallison.gth@gmail.com>
2018-07-17 17:13:45 -0300

    Added the "NgxGalleryModule" to the "imports" section of the "product-details.module.ts" file

--- web/src/main/webapp/src/app/pages/product-details/product-details.module.ts
+++ web/src/main/webapp/src/app/pages/product-details/product-details.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
+import { NgxGalleryModule } from 'ngx-gallery';
 
 import { ProductDetailsRoutingModule } from './product-details-routing.module';
 import { SharedModule } from '../../shared/shared.module';
@@ -13,6 +14,7 @@ import { ProductDetailsComponent } from './product-details.component';
     CommonModule,
     ButtonModule,
     TableModule,
+    NgxGalleryModule,
     SharedModule,
     ProductDetailsRoutingModule
   ],
