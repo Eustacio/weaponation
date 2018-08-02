72d95ae1e98dbeb0c44adada1c97470899d2b095
Eustácio <wallison.gth@gmail.com>
2018-08-02 17:48:02 -0300

    Changed the "SqlConverterMiddleware#from_crawler" method, to receive an signal when an "Product" is scraped

--- util/src/main/python/data_collector/data_collector/middlewares/sql_converter_middleware.py
+++ util/src/main/python/data_collector/data_collector/middlewares/sql_converter_middleware.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-
+from scrapy import signals
 from scrapy.crawler import Crawler
+
+from data_collector.model.product import Product
 
 
 class SqlConverterMiddleware:
@@ -10,4 +12,12 @@ class SqlConverterMiddleware:
 
     @classmethod
     def from_crawler(cls, crawler: Crawler):
+        # Connect to the Scrapy "item_scraped" signal, to receive
+        # the scraped item from the spider.
+        crawler.signals.connect(signal=signals.item_scraped, receiver=cls._item_scraped)
+
         return cls(crawler.settings.getbool('OUTPUT_SQL'))
+
+    @staticmethod
+    def _item_scraped(item: Product):
+        pass
