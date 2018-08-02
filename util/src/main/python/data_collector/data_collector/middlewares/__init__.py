b3d71fafc8301c7aa9c65891cb1d19fae9f509b9
Eustácio <wallison.gth@gmail.com>
2018-08-02 14:10:11 -0300

    Added import statement to the "__init__.py" file of the "middlewares" folder, so that Scrapy can find the "SqlConverterMiddleware" middleware

---
+++ util/src/main/python/data_collector/data_collector/middlewares/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# Custom Middleware must be imported here, so that Scrapy can find them
+
+from data_collector.middlewares.sql_converter_middleware import SqlConverterMiddleware
