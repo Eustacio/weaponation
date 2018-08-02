b5d32cd398bd6ad4d1c13f7e6c2233a96984b7a3
Eustácio <wallison.gth@gmail.com>
2018-08-02 16:19:42 -0300

    Changed the "SqlConverterMiddleware" Python class, to get the "OUTPUT_SQL" parameter from the command line

--- util/src/main/python/data_collector/data_collector/middlewares/sql_converter_middleware.py
+++ util/src/main/python/data_collector/data_collector/middlewares/sql_converter_middleware.py
@@ -1,2 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from scrapy.crawler import Crawler
+
+
 class SqlConverterMiddleware:
-    pass
+
+    def __init__(self, output_sql: bool = False) -> None:
+        self.output_sql: bool = output_sql
+
+    @classmethod
+    def from_crawler(cls, crawler: Crawler):
+        settings = crawler.settings
+        return cls(settings.getbool('OUTPUT_SQL'))
