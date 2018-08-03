78068c985b3f932366f71fc0364c34ab207b619d
Eustácio <wallison.gth@gmail.com>
2018-08-03 13:39:08 -0300

    Added abstract methods to "ProductSpider" Python class, to return all necessary data to create an instance of the "Product" class

--- util/src/main/python/data_collector/data_collector/middlewares/product_spider.py
+++ util/src/main/python/data_collector/data_collector/middlewares/product_spider.py
@@ -1,7 +1,37 @@
 # -*- coding: utf-8 -*-
 
-from abc import ABC
+from abc import ABC, abstractmethod
+from typing import List
+
+from scrapy.http import Response
 
 
 class ProductSpider(ABC):
-    pass
+
+    @abstractmethod
+    def _extract_name(self, response: Response) -> str:
+        pass
+
+    @abstractmethod
+    def _extract_manufacturer(self, response: Response) -> str:
+        pass
+
+    @abstractmethod
+    def _extract_description(self, response: Response) -> str:
+        pass
+
+    @abstractmethod
+    def _extract_specifications(self, response: Response) -> List[dict]:
+        pass
+
+    @abstractmethod
+    def _extract_price(self, response: Response) -> str:
+        pass
+
+    @abstractmethod
+    def _extract_category(self, response: Response) -> List[str]:
+        pass
+
+    @abstractmethod
+    def _extract_images(self, response: Response) -> List[dict]:
+        pass
